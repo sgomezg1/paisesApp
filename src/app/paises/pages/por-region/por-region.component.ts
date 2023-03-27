399dc775613020a53ba2544a794b7c10d34066b5
Sebastian Gomez <sebasgomez5892@gmail.com>
2023-03-26 23:53:46 -0500

    Por region commit

--- src/app/paises/pages/por-region/por-region.component.ts
+++ src/app/paises/pages/por-region/por-region.component.ts
@@ -1,38 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Country } from '../../interfaces/pais.interface';
-import { PaisService } from '../../services/pais.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
-  ]
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+  `]
 })
-export class PorRegionComponent implements OnInit {
-  termino: string = '';
-  hayError: boolean = false;
-  regiones: Country[] = [];
+export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
+  regionActiva: string = ''
 
-  constructor(private paisServ: PaisService) {}
+  activarRegion(region: string) {
+    this.regionActiva = region
+    // TODO: hacer llamado al servicio 
 
-  buscar(termino: string) {
-    this.termino = termino;
-    this.hayError = false;
-    this.paisServ.buscarRegion(this.termino).subscribe(
-      (regiones) => (this.regiones = regiones),
-      (err) => {
-        this.hayError = true;
-        this.regiones = [];
-      }
-    );
   }
-
-  sugerencias(termino: string) {
-    this.hayError = false
-    // TODO: Crear sugerencias
-  }
-
-  ngOnInit(): void {
-  }
-
 }
